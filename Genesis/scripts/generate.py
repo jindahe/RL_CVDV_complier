bc9714379eb442d3b0d46243a75a3e0b4734c142
jindahe <15067613031@163.com>
2025-12-11 13:07:51 +0800

    111

--- Genesis/scripts/generate.py
+++ Genesis/scripts/generate.py
@@ -1,7 +1,7 @@
 from itertools import permutations
 import numpy as np
 
-nums = ['+1', '+1', '-0', '-0', '-0']
+nums = ['-2', '-2', '-1', '-0', '-0']
 
 seen = set()
 res = []
@@ -10,7 +10,7 @@ for p in permutations(nums, len(nums)):
     if p in seen:
         continue
     seen.add(p)
-    s = "[" + "".join(str(x) for x in p) + "]" 
+    s = "[" + "".join(str(x) for x in p) + "]" + ", 170, 3, 3232, 596, 3828, 1538, 11266"
     res.append(s)
 
 for s in res:
